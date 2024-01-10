cc61063b34a0801f790033a3b47a0781b00185db
Noah Walz <noah@noahwalz.com>
2024-01-10 18:33:04 +0100

    check

--- src/components/Body/Body.tsx
+++ src/components/Body/Body.tsx
@@ -33,7 +33,7 @@ const Body: React.FC<Body> = () => {
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const response = await fetch("/.netlify/functions/clans/endzone", {
+        const response = await fetch("/functions/clans/endzone", {
           headers: {
             Authorization: `Bearer ${accessToken}`,
             "Content-Type": "application/json",
