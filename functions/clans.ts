c073c3693fc01cf504cdcd5adca5dd5cb88ef5d8
Noah Walz <noah@noahwalz.com>
2024-01-10 17:46:08 +0100

    changes

--- functions/clans.ts
+++ functions/clans.ts
@@ -3,66 +3,68 @@ import axios from 'axios';
 import { Request, Response } from 'express';
 import { accessToken } from "../src/service/proxy";
 import cors from "cors";  
+import { Handler, HandlerResponse, HandlerResponse as ServerlessHandlerResponse} from '@netlify/functions';
 import serverless from 'serverless-http';
-import { Handler } from '@netlify/functions';
+
 const app = express();
 
 const endzone = "#2YPY9PLUU";
 const firstzone = "#2YQQ80QGL";
 const secondzone = "#2QQPYRRCU";
-app.use(cors({ origin: '*' }));
+app.use(cors());
 
-app.get(`https://api-endzone-clan.netlify.app/clans/endzone`, async (req: Request, res: Response) => {
+app.get(`/clans/endzone`, async (req: Request, res: Response) => {
   try {
-      const response = await axios.get(`https://api.clashofclans.com/v1/clans/${encodeURIComponent(endzone)}`, {
-        headers: {
-          Authorization: `Bearer ${accessToken}`,
-          "Content-Type": "application/json",
-          mode: "no-cors",
-        },
-      });
-      const data = response.data;
+    const response = await axios.get(`https://api.clashofclans.com/v1/clans/${encodeURIComponent(endzone)}`, {
+      headers: {
+        Authorization: `Bearer ${accessToken}`,
+        "Content-Type": "application/json",
+        mode: "no-cors",
+      },
+    });
+    const data = response.data;
     res.json(data);
-
-  } catch (error) {
-    console.error('Error fetching data from Clash of Clans API:', error);
-    res.status(500).json({ error: 'Internal Server Error' });
-  }
-});
-app.get(`https://api-endzone-clan.netlify.app/clans/secondzone`, async (req: Request, res: Response) => {
-  try {
-      const response = await axios.get(`https://api.clashofclans.com/v1/clans/${encodeURIComponent(secondzone)}`, {
-        headers: {
-          Authorization: `Bearer ${accessToken}`,
-          "Content-Type": "application/json",
-          mode: "no-cors",
-        },
-      });
-      const data = response.data;
-    res.json(data);
-
-  } catch (error) {
-    console.error('Error fetching data from Clash of Clans API:', error);
-    res.status(500).json({ error: 'Internal Server Error' });
-  }
-});
-app.get(`https://api-endzone-clan.netlify.app/clans/firstzone`, async (req: Request, res: Response) => {
-  try {
-      const response = await axios.get(`https://api.clashofclans.com/v1/clans/${encodeURIComponent(firstzone)}`, {
-        headers: {
-          Authorization: `Bearer ${accessToken}`,
-          "Content-Type": "application/json",
-          mode: "no-cors",
-        },
-      });
-      const data = response.data;
-    res.json(data);
-
   } catch (error) {
     console.error('Error fetching data from Clash of Clans API:', error);
     res.status(500).json({ error: 'Internal Server Error' });
   }
 });
 
+app.get(`/clans/secondzone`, async (req: Request, res: Response) => {
+  try {
+    const response = await axios.get(`https://api.clashofclans.com/v1/clans/${encodeURIComponent(secondzone)}`, {
+      headers: {
+        Authorization: `Bearer ${accessToken}`,
+        "Content-Type": "application/json",
+        mode: "no-cors",
+      },
+    });
+    const data = response.data;
+    res.json(data);
+  } catch (error) {
+    console.error('Error fetching data from Clash of Clans API:', error);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
 
-module.exports = app;
+app.get(`/clans/firstzone`, async (req: Request, res: Response) => {
+  try {
+    const response = await axios.get(`https://api.clashofclans.com/v1/clans/${encodeURIComponent(firstzone)}`, {
+      headers: {
+        Authorization: `Bearer ${accessToken}`,
+        "Content-Type": "application/json",
+        mode: "no-cors",
+      },
+    });
+    const data = response.data;
+    res.json(data);
+  } catch (error) {
+    console.error('Error fetching data from Clash of Clans API:', error);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
+
+export const clansHandler: Handler = async (event, context): Promise<HandlerResponse> => {
+  const response: any = await serverless(app)(event, context);
+  return response;
+};
