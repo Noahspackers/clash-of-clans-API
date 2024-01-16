31fe89a9b6990d1fe6f00c0334456180018a07e4
Noah Walz <noah@noahwalz.com>
2024-01-16 19:28:57 +0100

    check

--- netfliy/functions/server.ts
+++ netfliy/functions/server.ts
@@ -1,4 +1,3 @@
-import { accessToken } from '../../src/service/accestoken';
 
 const client = require("clash-of-clans-node");
 
@@ -19,7 +18,7 @@ const secondzone = "#2QQPYRRCU";
 async function myFunction(): Promise<void> {
   await client.login({ email: 'noah.walz158@gmail.com', password: 'bopba5-fuwryj-nyczEx' });
 
-  await client.login(`${accessToken}`);
+  await client.login("eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiIsImtpZCI6IjI4YTMxOGY3LTAwMDAtYTFlYi03ZmExLTJjNzQzM2M2Y2NhNSJ9.eyJpc3MiOiJzdXBlcmNlbGwiLCJhdWQiOiJzdXBlcmNlbGw6Z2FtZWFwaSIsImp0aSI6ImVlNTUxYzMzLWFiMDAtNGZiNC04MGQ2LWMxMDA3YzE1MTI1OCIsImlhdCI6MTcwNDc1NTEzOSwic3ViIjoiZGV2ZWxvcGVyLzllODZjN2U4LTI2ZjctNjRmOC0zOTE4LWQ3MGQ2MzM1Y2FjMyIsInNjb3BlcyI6WyJjbGFzaCJdLCJsaW1pdHMiOlt7InRpZXIiOiJkZXZlbG9wZXIvc2lsdmVyIiwidHlwZSI6InRocm90dGxpbmcifSx7ImNpZHJzIjpbIjgxLjE2OS4xNDUuMTYzIiwiNjIuMTU3LjY1LjIxMCJdLCJ0eXBlIjoiY2xpZW50In1dfQ.PXrA97dIdLu4_vheurBmDnJoiTjxriFjEg3x0iOwCN9DCCZRj1oNZNuanj1rFIIVKfumsSg9_0bquvFWf9xiMw");
   const clan: Clan = await client.getClan(`${encodeURIComponent(endzone)}`);
   console.log(`${clan.name} (${clan.tag})`);
 }
