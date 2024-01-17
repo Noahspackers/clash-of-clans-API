b95c0a389cbedcc851c991a4bfd4b3f2e4b77f06
Noah Walz <noah@noahwalz.com>
2024-01-17 20:25:48 +0100

    check agin

--- netfliy/functions/api.js
+++ netfliy/functions/api.js
@@ -1,3 +1,4 @@
+const { response } = require('express');
 const { accessToken } = require('../../src/service/accestoken');
 const client = require("clash-of-clans-node");
 
@@ -7,12 +8,17 @@ const secondzone = "#2QQPYRRCU";
 
 exports.handler = async function () {
   await client.login("eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiIsImtpZCI6IjI4YTMxOGY3LTAwMDAtYTFlYi03ZmExLTJjNzQzM2M2Y2NhNSJ9.eyJpc3MiOiJzdXBlcmNlbGwiLCJhdWQiOiJzdXBlcmNlbGw6Z2FtZWFwaSIsImp0aSI6ImVlNTUxYzMzLWFiMDAtNGZiNC04MGQ2LWMxMDA3YzE1MTI1OCIsImlhdCI6MTcwNDc1NTEzOSwic3ViIjoiZGV2ZWxvcGVyLzllODZjN2U4LTI2ZjctNjRmOC0zOTE4LWQ3MGQ2MzM1Y2FjMyIsInNjb3BlcyI6WyJjbGFzaCJdLCJsaW1pdHMiOlt7InRpZXIiOiJkZXZlbG9wZXIvc2lsdmVyIiwidHlwZSI6InRocm90dGxpbmcifSx7ImNpZHJzIjpbIjgxLjE2OS4xNDUuMTYzIiwiNjIuMTU3LjY1LjIxMCJdLCJ0eXBlIjoiY2xpZW50In1dfQ.PXrA97dIdLu4_vheurBmDnJoiTjxriFjEg3x0iOwCN9DCCZRj1oNZNuanj1rFIIVKfumsSg9_0bquvFWf9xiMw");
-  const clan = await client.getClan(`${encodeURIComponent(endzone)}`);
+  const clan = await client.getClan(`${encodeURIComponent(endzone)}`, {
+    headers: {
+      Authorization: `Bearer ${accessToken}`,
+    }
+  });
   console.log(`${clan.name} (${clan.tag})`);
+  clan = response.data;
   return {
     statusCode: 200,
     body: JSON.stringify({
-      message: "Hello world",
+     response: response.data
     }),
   };
   };
