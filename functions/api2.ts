ba0820546608efc2271a36d7e1ea6b8781f6de63
Noah Walz <noah@noahwalz.com>
2024-01-19 15:19:16 +0100

    check

--- functions/api2.ts
+++ functions/api2.ts
@@ -24,13 +24,12 @@ const API = "https://api.clashofclans.com/v1";
 
 exports.handler = async function (event, context) {
 
-  console.log('Incoming request to /api from Netlify:', event.path);
   const clans = `${API}/clans/${encodeURIComponent(endzone)}`;
   const headers = new Headers();
   headers.set("Authorization", `Bearer ${accessToken}`);
   const response = await fetch(clans, { headers });
   const data = await response.json();
-  console.log()
+  console.log(response)
   return {
     statusCode: 200,
     body: JSON.stringify({
