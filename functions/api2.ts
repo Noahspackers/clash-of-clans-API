882e394c2a3b5b810e2f82bb06e5979b2b8bdf47
Noah Walz <noah@noahwalz.com>
2024-01-19 15:53:59 +0100

    check

--- functions/api2.ts
+++ functions/api2.ts
@@ -10,19 +10,29 @@ const API = "https://api.clashofclans.com/v1";
 
 exports.handler = async function (event, context) {
   // Redirect /api2 to the target URL
-  if (event.path === '/api2') {
+  if (event.path === '/.netlify/functions/api2') {
     const clans = `${API}/clans/${encodeURIComponent(endzone)}`;
     const headers = new Headers();
     headers.set('Authorization', `Bearer ${accessToken}`);
     const response = await fetch(clans, { headers });
-    const data = await response.json();
-    return {
-      statusCode: 200,
-      body: JSON.stringify({ data }),
-    };
+
+    if (response.ok) {
+      const data = await response.json();
+      return {
+        statusCode: 200,
+        body: JSON.stringify({ data }),
+      };
+    } else {
+      return {
+        statusCode: response.status,
+        body: JSON.stringify({ error: `API request failed with status ${response.status}` }),
+      };
+    }
   }
+
+  // If no matching route, return a 404 response
   return {
     statusCode: 404,
     body: JSON.stringify({ error: 'Not Found' }),
   };
-}
+};
