137862829519fd367d4eb8d11c7735e030540d06
Noah Walz <noah@noahwalz.com>
2024-01-19 02:20:18 +0100

    check

--- functions/api2.ts
+++ functions/api2.ts
@@ -6,30 +6,9 @@ const firstzone = "#2YQQ80QGL";
 const secondzone = "#2QQPYRRCU";
 
 
-// Get the network interfaces
-const networkInterfaces = os.networkInterfaces();
-
-// Check if networkInterfaces is defined and not null
-if (networkInterfaces) {
-  // Extract the IPv4 addresses from all network interfaces
-  const ipv4Addresses = Object.values(networkInterfaces)
-    .flat()
-    .filter((info) => info?.family === 'IPv4')
-    .map((info) => info?.address)
-    .filter((address) => address !== undefined);
-
-  if (ipv4Addresses.length > 0) {
-    console.log(`IPv4 Addresses: ${ipv4Addresses.join(', ')}`);
-  } else {
-    console.log('No IPv4 addresses found');
-  }
-} else {
-  console.log('Network interfaces not available');
-}
-
 const API = "https://api.clashofclans.com/v1";
 
-module.exports.handler = async function (event, context) {
+exports.handler = async function (event, context) {
   console.log('Incoming request to /api from Netlify:', event.path);
   const clans = `${API}/clans/${encodeURIComponent(endzone)}`;
   const headers = new Headers();
