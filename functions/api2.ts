071ef134030e958145142f005740ec0254e2a491
Noah Walz <noah@noahwalz.com>
2024-01-19 12:14:51 +0100

    checking

--- functions/api2.ts
+++ functions/api2.ts
@@ -1,14 +1,28 @@
 import fetch, { Headers } from "node-fetch";
 import os from "os";
 import { accessToken } from "../src/service/accestoken";
+import { Router } from 'express';
+import { createProxyMiddleware } from 'http-proxy-middleware'
+
 const endzone = "#2YPY9PLUU";
 const firstzone = "#2YQQ80QGL";
 const secondzone = "#2QQPYRRCU";
 
+const router = Router();
+
+const options = {
+    target: 'https://endzone-clan.de', // target host
+    changeOrigin: true, // needed for virtual hosted sites
+    pathRewrite: {
+       [`^/api2`]: '',
+    }, // rewrites our endpoints to '' when forwarded to our target
+}
+router.get('/api2', createProxyMiddleware(options));
 
 const API = "https://api.clashofclans.com/v1";
 
 exports.handler = async function (event, context) {
+
   console.log('Incoming request to /api from Netlify:', event.path);
   const clans = `${API}/clans/${encodeURIComponent(endzone)}`;
   const headers = new Headers();
