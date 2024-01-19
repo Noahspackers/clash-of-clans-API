534a882e888941345f5e740dc6ccb39577a3f712
Noah Walz <noah@noahwalz.com>
2024-01-19 16:08:03 +0100

    chck

--- functions/api2.ts
+++ functions/api2.ts
@@ -1,38 +1,39 @@
 import fetch, { Headers } from "node-fetch";
+import os from "os";
 import { accessToken } from "../src/service/accestoken";
+import { Router } from 'express';
+import { createProxyMiddleware } from 'http-proxy-middleware'
 
 const endzone = "#2YPY9PLUU";
 const firstzone = "#2YQQ80QGL";
 const secondzone = "#2QQPYRRCU";
 
+const router = Router();
+
+const options = {
+    target: "http://api.clashofclans.com/v1",
+    changeOrigin: true, // needed for virtual hosted sites
+    pathRewrite: {
+       [`^/.netlify/functions/api2`]: '/',
+    },
+  // rewrites our endpoints to '' when forwarded to our target
+}
+router.get('/api2', createProxyMiddleware(options));
 
 const API = "https://api.clashofclans.com/v1";
 
 exports.handler = async function (event, context) {
-  // Redirect /api2 to the target URL
-  if (event.path === '/.netlify/functions/api2') {
-    const clans = `${API}/clans/${encodeURIComponent(endzone)}`;
-    const headers = new Headers();
-    headers.set('Authorization', `Bearer ${accessToken}`);
-    const response = await fetch(clans, { headers });
 
-    if (response.ok) {
-      const data = await response.json();
-      return {
-        statusCode: 200,
-        body: JSON.stringify({ data }),
-      };
-    } else {
-      return {
-        statusCode: response.status,
-        body: JSON.stringify({ error: `API request failed with status ${response.status}` }),
-      };
-    }
-  }
-
-  // If no matching route, return a 404 response
+  const clans = `${API}/clans/${encodeURIComponent(endzone)}`;
+  const headers = new Headers();
+  headers.set("Authorization", `Bearer ${accessToken}`);
+  const response = await fetch(clans, { headers });
+  const data = await response.json();
+  console.log(data.name)
   return {
-    statusCode: 404,
-    body: JSON.stringify({ error: 'Not Found' }),
+    statusCode: 200,
+    body: JSON.stringify({
+      data
+    }),
   };
-};
+}
