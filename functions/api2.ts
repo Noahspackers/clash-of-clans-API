2724903f7861d8b7054f41e7bb308d8e4ab78b27
Noah Walz <noah@noahwalz.com>
2024-01-23 19:19:28 +0100

    heck

--- functions/api2.ts
+++ functions/api2.ts
@@ -1,8 +1,7 @@
 import fetch, { Headers } from "node-fetch";
-import os from "os";
 import { accessToken } from "../src/service/accestoken";
 import { Router } from 'express';
-import { createProxyMiddleware } from 'http-proxy-middleware'
+import { createProxyMiddleware } from 'http-proxy-middleware';
 
 const clan = "#2YPY9PLUU";
 const firstzone = "#2YQQ80QGL";
@@ -14,24 +13,44 @@ const options = {
     target: "http://api.clashofclans.com/v1",
     changeOrigin: true, 
     pathRewrite: {
-       [`^/.netlify/functions/api2`]: '',
+        [`^/.netlify/functions/api2`]: '',
     },
-}
+    onProxyReq: (proxyReq, req) => {
+        proxyReq.setHeader("Authorization", `Bearer ${accessToken}`);
+        proxyReq.setHeader("Host", "api.clashofclans.com");
+        proxyReq.setHeader("X-Forwarded-For", "62.157.65.210");
+        proxyReq.setHeader("X-Real-IP", "62.157.65.210");
+    },
+};
 
 const API = "https://api.clashofclans.com/v1";
 
 exports.handler = async function (event, context) {
-  router.get('/api2', createProxyMiddleware(options));
-  const clans = `${API}/clans/${encodeURIComponent(clan)}`;
-  const headers = new Headers();
-  headers.set("Authorization", `Bearer ${accessToken}`);
-  const response = await fetch(clans, { headers });
-  const data = await response.json();
-  console.log(data.name)
-  return {
-    statusCode: 200,
-    body: JSON.stringify({
-      data
-    }),
-  };
-}
+    router.use('/api2', createProxyMiddleware(options));
+    
+    try {
+        const clans = `${API}/clans/${encodeURIComponent(clan)}`;
+        const headers = new Headers();
+        headers.set("Authorization", `Bearer ${accessToken}`);
+
+        const response = await fetch(clans, { headers });
+        const data = await response.json();
+
+        console.log(data.name);
+
+        return {
+            statusCode: 200,
+            body: JSON.stringify({
+                data,
+            }),
+        };
+    } catch (error) {
+        console.error(error);
+        return {
+            statusCode: 500,
+            body: JSON.stringify({
+                error: "Internal Server Error",
+            }),
+        };
+    }
+};
